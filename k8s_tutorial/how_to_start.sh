e1ff3646a2478117f0b4c1ce058ba2a5fdd2d945
metraf <firsov.tyoma@gmail.com>
2022-01-23 23:51:36 +0100

    init

---
+++ k8s_tutorial/how_to_start.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+echo "starting minikube"
+minikube start --driver docker
+kubectl get node
+
+echo "applying yaml files"
+kubectl apply -f mongo-config.yaml
+kubectl apply -f mongo-secret.yaml
+kubectl apply -f mongo.yaml
+kubectl apply -f webapp.yaml
+
+echo "checking how the pods working"
+kubectl get all
